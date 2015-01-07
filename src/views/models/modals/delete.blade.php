42fa24cb141bb9204e4f71756018327a69751d89
Jonathan Bonnefoy <contact@e-weap.fr>
2015-01-07 17:13:07 +0100

    Raw modal FR translation

--- src/views/models/modals/delete.blade.php
+++ src/views/models/modals/delete.blade.php
@@ -1,17 +1,17 @@
 <div class="modal-header">
-	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	<h4 class="modal-title">Are you sure you want to continue?</h4>
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h4 class="modal-title">Êtes-vous sûr de vouloir continuer ?</h4>
 </div>
 <div class="modal-body">
-	<p>This action is irreversible.</p>
-	<button type="button" class="btn btn-default btn-rounded" data-dismiss="modal">Cancel</button>
-	<button type="button" id="post-delete" class="btn btn-default btn-red btn-rounded">
-		Yes, delete the {{ $model->getSingularName() }}
-	</button>
+    <p>Les éléments seront définitivement supprimés</p>
+    <button type="button" class="btn btn-default btn-rounded" data-dismiss="modal">Annuler</button>
+    <button type="button" id="post-delete" class="btn btn-default btn-red btn-rounded">
+        Oui, supprimer les {{ $model->getSingularName() }}
+    </button>
 </div>
 
 <script>
-	$('#post-delete').click(function () {
-		$('#delete-multi-form').submit();
-	});
+    $('#post-delete').click(function () {
+        $('#delete-multi-form').submit();
+    });
 </script>
